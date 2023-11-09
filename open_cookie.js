eb3339e99a6d61e0b7501f8266090c3819cf26b9
Pukia <70846484+Pukia-vj@users.noreply.github.com>
2023-11-09 16:05:11 +0800

    Update open_cookie.js

--- open_cookie.js
+++ open_cookie.js
@@ -1,1 +1,15 @@
-1
+const cookieName = 'open'
+const cookieKey = 'vvv_cookie_open'
+const cookieVal = $request.headers['Cookie'] || $request.headers['cookie']
+
+if (cookieVal) {
+  let cookie = $prefs.setValueForKey(cookieVal, cookieKey)
+  if (cookie) {
+    let msg = `${cookieName}`
+    $notify(msg, 'Cookie写入成功', '详见日志')
+    console.log(msg)
+    console.log(cookieVal)
+  }
+}
+
+$done({})
