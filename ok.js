1bae35c3ba1e1dfef7f7a135fffe4c5e1f7fb124
Pukia <70846484+Pukia-vj@users.noreply.github.com>
2023-11-09 11:42:16 +0800

    Create ok.js

---
+++ ok.js
@@ -0,0 +1,30 @@
+const cookieName = 'okpt'
+const cookieKey = 'chavy_cookie_okpt'
+const cookieVal = $prefs.valueForKey(cookieKey)
+
+function signMission(code) {
+  let url = {
+    url: `https://www.v2ex.com/mission/daily/redeem?once=${code}`,
+    method: 'GET',
+    headers: { Cookie: cookieVal }
+  }
+  $task.fetch(url).then((response) => {
+    let data = response.body
+    if (data.indexOf('每日登录奖励已领取') >= 0) {
+      let title = `${cookieName}`
+      let subTitle = `签到结果: 签到成功`
+      let detail = `今天已经签过了`
+      console.log(`${title}, ${subTitle}, ${detail}`)
+      $notify(title, subTitle, detail)
+    } else {
+      let title = `${cookieName}`
+      let subTitle = `签到结果: 签到失败`
+      let detail = `详见日志`
+      console.log(`签到失败: ${cookieName}, data: ${data}`)
+      $notify(title, subTitle, detail)
+    }
+    $done()
+  })
+}
+
+sign({})
