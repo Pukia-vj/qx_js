af4ba79b82a1e3c94326f94eb56dac81e4798cbf
Pukia <70846484+Pukia-vj@users.noreply.github.com>
2023-11-10 10:59:15 +0800

    Create tjupt.js

---
+++ tjupt.js
@@ -0,0 +1,30 @@
+const cookieName = 'tjupt'
+const cookieKey = 'vvv_cookie_tjupt'
+const cookieVal = $prefs.valueForKey(cookieKey)
+
+function sign(code) {
+  let url = {
+    url: `https://www.tjupt.org/index.php`,
+    method: 'GET',
+    headers: { Cookie: cookieVal }
+  }
+  $task.fetch(url).then((response) => {
+    let data = response.body
+    if (data.indexOf('上传量') >= 0) {
+      let title = `${cookieName}`
+      let subTitle = `登录结果: 登录成功`
+      let detail = `北洋园`
+      console.log(`${title}, ${subTitle}, ${detail}`)
+      $notify(title, subTitle, detail)
+    } else {
+      let title = `${cookieName}`
+      let subTitle = `登录结果: 北洋园登录失败`
+      let detail = `详见日志`
+      console.log(`登录失败: ${cookieName}, data: ${data}`)
+      $notify(title, subTitle, detail)
+    }
+    $done()
+  })
+}
+
+sign({})
