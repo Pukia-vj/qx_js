7f568720e12e7420be5105eba8e194e82249c07c
Pukia <70846484+Pukia-vj@users.noreply.github.com>
2023-11-10 09:56:26 +0800

    Create discfan.js

---
+++ discfan.js
@@ -0,0 +1,30 @@
+const cookieName = 'cdiscfan'
+const cookieKey = 'vvv_cookie_discfan'
+const cookieVal = $prefs.valueForKey(cookieKey)
+
+function sign(code) {
+  let url = {
+    url: `https://discfan.net/attendance.php`,
+    method: 'GET',
+    headers: { Cookie: cookieVal }
+  }
+  $task.fetch(url).then((response) => {
+    let data = response.body
+    if (data.indexOf('签到成功') >= 0) {
+      let title = `${cookieName}`
+      let subTitle = `签到结果: 签到成功`
+      let detail = `碟粉`
+      console.log(`${title}, ${subTitle}, ${detail}`)
+      $notify(title, subTitle, detail)
+    } else {
+      let title = `${cookieName}`
+      let subTitle = `签到结果: 碟粉签到失败`
+      let detail = `详见日志`
+      console.log(`签到失败: ${cookieName}, data: ${data}`)
+      $notify(title, subTitle, detail)
+    }
+    $done()
+  })
+}
+
+sign({})
