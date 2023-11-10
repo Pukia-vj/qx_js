0a58e0eadc86afc0620b2608efe0d8a669308ae5
Pukia <70846484+Pukia-vj@users.noreply.github.com>
2023-11-10 09:47:01 +0800

    Create chd.js

---
+++ chd.js
@@ -0,0 +1,30 @@
+const cookieName = 'ptchdbits'
+const cookieKey = 'vvv_cookie_ptchdbits'
+const cookieVal = $prefs.valueForKey(cookieKey)
+
+function sign(code) {
+  let url = {
+    url: `https://ptchdbits.co/index.php`,
+    method: 'GET',
+    headers: { Cookie: cookieVal }
+  }
+  $task.fetch(url).then((response) => {
+    let data = response.body
+    if (data.indexOf('种子') >= 0) {
+      let title = `${cookieName}`
+      let subTitle = `登录结果: 登录成功`
+      let detail = `彩虹岛`
+      console.log(`${title}, ${subTitle}, ${detail}`)
+      $notify(title, subTitle, detail)
+    } else {
+      let title = `${cookieName}`
+      let subTitle = `登录结果: 彩虹岛登录失败`
+      let detail = `详见日志`
+      console.log(`登录失败: ${cookieName}, data: ${data}`)
+      $notify(title, subTitle, detail)
+    }
+    $done()
+  })
+}
+
+sign({})
