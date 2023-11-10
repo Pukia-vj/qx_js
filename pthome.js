3956b8c2fe617dc33be18019d5c4dd33c7206a0b
Pukia <70846484+Pukia-vj@users.noreply.github.com>
2023-11-10 10:58:08 +0800

    Create pthome.js

---
+++ pthome.js
@@ -0,0 +1,30 @@
+const cookieName = 'pthome'
+const cookieKey = 'vvv_cookie_pthome'
+const cookieVal = $prefs.valueForKey(cookieKey)
+
+function sign(code) {
+  let url = {
+    url: `https://www.pthome.net/attendance.php`,
+    method: 'GET',
+    headers: { Cookie: cookieVal }
+  }
+  $task.fetch(url).then((response) => {
+    let data = response.body
+    if (data.indexOf('签到成功') >= 0) {
+      let title = `${cookieName}`
+      let subTitle = `签到结果: 签到成功`
+      let detail = `铂金家`
+      console.log(`${title}, ${subTitle}, ${detail}`)
+      $notify(title, subTitle, detail)
+    } else {
+      let title = `${cookieName}`
+      let subTitle = `签到结果: 铂金家签到失败`
+      let detail = `详见日志`
+      console.log(`签到失败: ${cookieName}, data: ${data}`)
+      $notify(title, subTitle, detail)
+    }
+    $done()
+  })
+}
+
+sign({})
