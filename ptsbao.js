479d41eab9bf0bad977eae275a66c55e4c8e85e8
Pukia <70846484+Pukia-vj@users.noreply.github.com>
2023-11-10 11:08:27 +0800

    Create ptsbao.js

---
+++ ptsbao.js
@@ -0,0 +1,30 @@
+const cookieName = 'ptsbao'
+const cookieKey = 'vvv_cookie_ptsbao'
+const cookieVal = $prefs.valueForKey(cookieKey)
+
+function sign(code) {
+  let url = {
+    url: `https://ptsbao.club/index.php`,
+    method: 'GET',
+    headers: { Cookie: cookieVal }
+  }
+  $task.fetch(url).then((response) => {
+    let data = response.body
+    if (data.indexOf('上传量') >= 0) {
+      let title = `${cookieName}`
+      let subTitle = `登录结果: 登录成功`
+      let detail = `烧包`
+      console.log(`${title}, ${subTitle}, ${detail}`)
+      $notify(title, subTitle, detail)
+    } else {
+      let title = `${cookieName}`
+      let subTitle = `登录结果: 烧包登录失败`
+      let detail = `详见日志`
+      console.log(`登录失败: ${cookieName}, data: ${data}`)
+      $notify(title, subTitle, detail)
+    }
+    $done()
+  })
+}
+
+sign({})
