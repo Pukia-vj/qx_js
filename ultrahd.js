101cb07d90a3a304d6d6a8ba51a2908a78000901
Pukia <70846484+Pukia-vj@users.noreply.github.com>
2023-11-10 11:13:00 +0800

    Create ultrahd.js

---
+++ ultrahd.js
@@ -0,0 +1,30 @@
+const cookieName = 'ultrahd'
+const cookieKey = 'vvv_cookie_ultrahd'
+const cookieVal = $prefs.valueForKey(cookieKey)
+
+function sign(code) {
+  let url = {
+    url: `https://ultrahd.net/attendance.php`,
+    method: 'GET',
+    headers: { Cookie: cookieVal }
+  }
+  $task.fetch(url).then((response) => {
+    let data = response.body
+    if (data.indexOf('签到成功') >= 0) {
+      let title = `${cookieName}`
+      let subTitle = `签到结果: 签到成功`
+      let detail = `ultrahd`
+      console.log(`${title}, ${subTitle}, ${detail}`)
+      $notify(title, subTitle, detail)
+    } else {
+      let title = `${cookieName}`
+      let subTitle = `签到结果: ultrahd签到失败`
+      let detail = `详见日志`
+      console.log(`签到失败: ${cookieName}, data: ${data}`)
+      $notify(title, subTitle, detail)
+    }
+    $done()
+  })
+}
+
+sign({})
