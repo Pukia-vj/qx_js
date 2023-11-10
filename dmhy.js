73e967f9b80952ea0e702746677cd38105862d16
Pukia <70846484+Pukia-vj@users.noreply.github.com>
2023-11-10 09:57:39 +0800

    Create dmhy.js

---
+++ dmhy.js
@@ -0,0 +1,30 @@
+const cookieName = 'dmhy'
+const cookieKey = 'vvv_cookie_dmhy'
+const cookieVal = $prefs.valueForKey(cookieKey)
+
+function sign(code) {
+  let url = {
+    url: `https://u2.dmhy.org/index.php`,
+    method: 'GET',
+    headers: { Cookie: cookieVal }
+  }
+  $task.fetch(url).then((response) => {
+    let data = response.body
+    if (data.indexOf('种子') >= 0) {
+      let title = `${cookieName}`
+      let subTitle = `登录结果: 登录成功`
+      let detail = `U2`
+      console.log(`${title}, ${subTitle}, ${detail}`)
+      $notify(title, subTitle, detail)
+    } else {
+      let title = `${cookieName}`
+      let subTitle = `登录结果: U2登录失败`
+      let detail = `详见日志`
+      console.log(`登录失败: ${cookieName}, data: ${data}`)
+      $notify(title, subTitle, detail)
+    }
+    $done()
+  })
+}
+
+sign({})
