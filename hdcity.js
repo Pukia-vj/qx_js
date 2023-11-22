f841ea1d11c7160006a3acba05ea699ca76ce3b7
Pukia <70846484+Pukia-vj@users.noreply.github.com>
2023-11-22 09:51:27 +0800

    Update hdcity.js

--- hdcity.js
+++ hdcity.js
@@ -4,23 +4,23 @@ const cookieVal = $prefs.valueForKey(cookieKey)
 
 function sign(code) {
   let url = {
-    url: `https://hdcity.leniter.org/`,
+    url: `https://hdcity.leniter.org/sign`,
     method: 'GET',
     headers: { Cookie: cookieVal }
   }
   $task.fetch(url).then((response) => {
     let data = response.body
-    if (data.indexOf('签到') >= 0) {
+    if (data.indexOf('签过到') >= 0) {
       let title = `${cookieName}`
-      let subTitle = `登录结果: 登录成功`
+      let subTitle = `签到结果: 签到成功`
       let detail = `城市`
       console.log(`${title}, ${subTitle}, ${detail}`)
       $notify(title, subTitle, detail)
     } else {
       let title = `${cookieName}`
-      let subTitle = `登录结果: 城市登录失败`
+      let subTitle = `签到结果: 城市签到失败`
       let detail = `详见日志`
-      console.log(`登录失败: ${cookieName}, data: ${data}`)
+      console.log(`签到失败: ${cookieName}, data: ${data}`)
       $notify(title, subTitle, detail)
     }
     $done()
